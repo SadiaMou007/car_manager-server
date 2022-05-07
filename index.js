b727d426057debf8a34a5f7f98695be02cfb999c
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-06 20:14:58 -0700

    delete api

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ async function run() {
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
